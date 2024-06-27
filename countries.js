b49d84dd8b58715ec0069be884800d7b3c2760df
sophie950 <sueboo950950@gmail.com>
2024-06-27 01:41:30 -0400

    update

--- countries.js
+++ countries.js
@@ -1,1 +1,9 @@
+function countriesCreator(){
+    let name, country;
+    name = prompt("What is your name?");
+    country = prompt("What country would you love to visit?");
+    countriesHeader.innerHTML = `Countries Visited`;
+    countriesParagraph.innerHTML = `Hi ${name}! I would love to visit Egypt one day. I sincerely hope that you get to visit ${country} one day as well.`
 
+}
+countriesButton.onclick = countriesCreator
--- placeslived.js
+++ placeslived.js
@@ -1,1 +1,9 @@
+function placeslivedCreator(){
+    let place, name;
+    name = prompt("What is your name?");
+    place = prompt("Where have you lived?");
+    placeslivedHeader.innerHTML = `Places Lived`;
+    placeslivedParagraph.innerHTML = `Hi ${name}! I am so glad to see that you have lived ${place}. I bet that you have had some amazing experiences while there. Never forget to keep exploring!`;
 
+}
+placeslivedButton.onclick = placeslivedCreator
