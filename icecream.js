2df7f99884a7915e41bcf76da5f7c4486c15df0e
sophie950 <sueboo950950@gmail.com>
2024-06-27 01:53:26 -0400

    update

--- icecream.js
+++ icecream.js
@@ -1,1 +1,9 @@
+function icecreamCreator(){
+    let name, flavor;
+    name = prompt("What is your name?");
+    flavor = prompt("What is your favorite flavor of ice cream?");
+    icecreamHeader.innerHTML = 'Ice Cream...What Is Your Flavor';
+    icecreamParagraph.innerHTML = `Hi ${name}! That is awesome that you love ${flavor} ice cream. I'll bet that it tastes amazing.`
 
+}
+icecreamButton.onclick = icecreamCreator
